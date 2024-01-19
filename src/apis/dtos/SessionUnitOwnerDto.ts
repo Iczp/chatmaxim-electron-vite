fa8540648ec234192994c5965a0021b1e54a3e2e
zhongpei <1000@intry.cn>
2024-01-19 11:05:08 +0800

    0

--- src/apis/dtos/SessionUnitOwnerDto.ts
+++ src/apis/dtos/SessionUnitOwnerDto.ts
@@ -1,21 +1,110 @@
+import { ChatObjectTypeEnums } from '../enums';
 import { ChatObjectDto } from './ChatObjectDto';
 import { MessageDto } from './MessageDto';
 import { SessionItemDto } from './SessionItemDto';
 import { SessionUnitSettingDto } from './SessionUnitSettingDto';
 
 export type SessionUnitOwnerDto = SessionItemDto & {
-  // id?: string;
+  /**
+   *
+   *
+   * @type {string}
+   */
+  id?: string;
+
+  /**
+   *
+   *
+   * @type {number}
+   */
   ownerId?: number;
-  // lastMessageId?: number | null;
-  // sorting?: number;
+
+  /**
+   *
+   *
+   * @type {ChatObjectTypeEnums}
+   */
+  ownerObjectType?: ChatObjectTypeEnums;
+
+  /**
+   *
+   *
+   * @type {(number | null)}
+   */
+  lastMessageId?: number | null;
+
+  /**
+   *
+   *
+   * @type {number}
+   */
+  sorting?: number;
+
+  /**
+   *
+   *
+   * @type {string}
+   */
   sessionId?: string;
+  /**
+   *
+   *
+   * @type {ChatObjectDto}
+   */
   destination?: ChatObjectDto;
+  /**
+   *
+   *
+   * @type {SessionUnitSettingDto}
+   */
   setting?: SessionUnitSettingDto;
+
+  /**
+   *
+   *
+   * @type {MessageDto}
+   */
   lastMessage?: MessageDto;
+
+  /**
+   *
+   *
+   * @type {number}
+   */
   publicBadge?: number;
+
+  /**
+   *
+   *
+   * @type {number}
+   */
   privateBadge?: number;
+
+  /**
+   *
+   *
+   * @type {number}
+   */
   remindAllCount?: number;
+
+  /**
+   *
+   *
+   * @type {number}
+   */
   remindMeCount?: number;
+
+  /**
+   *
+   *
+   * @type {number}
+   */
   followingCount?: number;
+  
+  /**
+   *
+   *
+   * @type {number}
+   */
   ticks?: number;
 };
